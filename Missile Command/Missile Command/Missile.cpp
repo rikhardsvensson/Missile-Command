43a9055df9487049487619afff3b9a5c976c2737
Rikhard Svensson <rille88@gmail.com>
2015-11-30 21:46:54 +0100

    ADDED empty missile class while i figure out how to update them properly.

---
+++ Missile Command/Missile Command/Missile.cpp
@@ -0,0 +1,21 @@
+#include "Missile.h"
+
+Missile::Missile()
+{
+
+}
+
+Missile::~Missile()
+{
+
+}
+
+void Missile::update()
+{
+
+}
+
+void Missile::render()
+{
+
+}
---
+++ Missile Command/Missile Command/Missile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Missile
+{
+public:
+	Missile();
+	~Missile();
+	void update();
+	void render();
+
+private:
+
+};
